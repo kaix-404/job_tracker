bd418a9ff5adc7f058336f8f6da2604a394813c4
kaix-404 <kairavnandi@gmail.com>
2026-04-11 11:01:01 +0530

    Prepared for deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,9 @@ import aiRoutes from "./routes/ai.routes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/applications", applicationRoutes);
